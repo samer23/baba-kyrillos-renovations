fdabbe32da139e4d2d34ca40523bbb85bfbec58d
samer23 <samerharten1@gmail.com>
2026-01-10 21:53:10 -0500

    Debug.

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken"
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization
+  console.log("authHeader: ", authHeader)
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Not authenticated" })
@@ -11,6 +12,7 @@ const authMiddleware = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    console.log("decoded: ", decoded)
     req.user = decoded
     next()
   } catch {
